b60401a37f84100b2f5abc01f5a201913567d4aa
KUDEX7KD <dkanaith987@gmail.com>
2026-09-21 18:20:21 +0530

    Update server.js
    Fix frontend backend connection

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://kudex7kd.github.io");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 app.get("/", (req, res) => {
   res.json({
